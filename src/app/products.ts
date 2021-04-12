07a554df1500e1d6fe4adefa256084b12904b9ec
New_computer <hromoi@inbox.ru>
2021-04-12 21:16:02 +0700

    'fix'

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,26 +1,26 @@
 export const products = [
     {
       name: 'rose',
-      image:'/assets/image/flowers/1.jpeg',  
+      image: '/assets/image/flowers/1.jpeg',
       price: 100000,
       description: 'There will text about flowers'
     },
     {
       name: 'rose',
-      image:'/assets/image/flowers/2.jpeg',        
+      image: '/assets/image/flowers/2.jpeg',
       price: 70000,
       description: 'A great phone with one '
     },
     {
       name: 'rose',
-      image:'/assets/image/flowers/3.jpeg',  
-      price:500000,
+      image: '/assets/image/flowers/3.jpeg',
+      price: 500000,
       description: 'There will text about flowers'
     },
     {
       name: 'rose',
-      image:'/assets/image/flowers/3.jpeg',  
-      price:570000,
+      image: '/assets/image/flowers/3.jpeg',
+      price: 570000,
       description: 'There will text about flowers'
     }
   ];
