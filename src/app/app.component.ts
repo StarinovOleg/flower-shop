058ad7905c981cf6b5183b79aedd2ba664c746cb
StarinovOleg <hromoi@inbox.ru>
2021-04-12 21:07:15 +0700

    fix header, add routing for show product details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import {Title} from '@angular/platform-browser';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-
 })
 export class AppComponent {
   title = 'Hoa Tươi Mai Phương';
   public constructor(private titleService: Title) { }
-
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
   }
+  showStepperComp: boolean = true;
 
+  showHideStepperComp(value:any) {
+    this.showStepperComp = value;
+  }
 }
