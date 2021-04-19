317c972304b2996b57c33441afc77c31c87e1d07
StarinovOleg <hromoi@inbox.ru>
2021-04-19 23:53:49 +0700

    product-details layout

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,23 +1,23 @@
 export const products = [
-    { id:0,
+    { id: 0,
       name: 'rose',
       image: '/assets/image/flowers/1.jpeg',
       price: 100000,
       description: 'There will text about flowers'
     },
-    { id:1,
+    { id: 1,
       name: 'rose',
       image: '/assets/image/flowers/2.jpeg',
       price: 70000,
       description: 'A great phone with one '
     },
-    { id:2,
+    { id: 2,
       name: 'rose',
       image: '/assets/image/flowers/3.jpeg',
       price: 500000,
       description: 'There will text about flowers'
     },
-    { id:3,
+    { id: 3,
       name: 'rose',
       image: '/assets/image/flowers/3.jpeg',
       price: 570000,
