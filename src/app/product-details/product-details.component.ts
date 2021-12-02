24ec75c5886634f9281561d736a005b4915c925e
StarinovOleg <hromoi@inbox.ru>
2021-12-02 12:53:54 +0700

    fix cart

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute,Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { Product } from '../models/product';
 
@@ -8,7 +8,7 @@ import { CartService } from '../services/cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
@@ -16,25 +16,22 @@ export class ProductDetailsComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private productsService: productsService,
-    private CartService: CartService,
-  ) { }
+    private CartService: CartService
+  ) {}
   ngOnInit(): void {
-    const id =Number(this.route.snapshot.paramMap.get('id'));
-      if (id) {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
       this.getAll(id);
     }
-
-
   }
-    getAll(id: number): void {
-      this.productsService.getAll(id).subscribe({
-         next:product => (this.product=product)
-      }
-      ) 
+  getAll(id: number): void {
+    this.productsService.getAll(id).subscribe({
+      next: (product) => (this.product = product),
+    });
   }
   addToCart(product: Product) {
     this.CartService.addToCart(product);
-    this.router.navigate([ '/shop-cards' ])
+    this.router.navigate(['/product-catalog']);
     //window.alert('Flower has been added tp the cart')
   }
 }
--- src/app/product-shoping/cart/cart.component.ts
+++ src/app/product-shoping/cart/cart.component.ts
@@ -1,36 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { Product } from '../../models/product';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  items=this.CartService.getItems();
+  items = this.CartService.getItems();
   result: string | undefined;
-  empty="Here will all your orders";
+  empty = 'Here will all your orders';
   form!: FormGroup;
   constructor(
     private CartService: CartService,
     private formBuilder: FormBuilder
-  ) { }
+  ) {
+    this.form = new FormGroup({
+      name: new FormControl('Tom', Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[a-zA-Z_]+@[a-zA-Z_]+?.[a-zA-Z]{2,3}'),
+      ]),
+    });
+  }
 
   ngOnInit(): void {
-    this.form =this.formBuilder.group({
+    this.form = this.formBuilder.group({
       name: [null, Validators.required],
-      phone: [null,Validators.required],
-      address: [null, Validators.required]
+      phone: [null, Validators.required],
+      address: [null, Validators.required],
     });
   }
   clearCart(item: Product) {
     this.CartService.clearCart();
   }
-    onSubmit(): void {
+  onSubmit(): void {
     // Process checkout data here
-    this.CartService.clearCart(); 
-    this.result="success";
-    this.empty="";
+    this.CartService.clearCart();
+    this.result = 'success';
+    this.empty = '';
   }
 }
