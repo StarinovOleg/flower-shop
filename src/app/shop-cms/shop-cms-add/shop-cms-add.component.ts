f918e418c089b3aaaaf659c4d8799e69b9233fa5
StarinovOleg <hromoi@inbox.ru>
2021-12-16 15:59:19 +0700

    add upload service

--- src/app/shop-cms/shop-cms-add/shop-cms-add.component.ts
+++ src/app/shop-cms/shop-cms-add/shop-cms-add.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { HttpEventType, HttpErrorResponse } from '@angular/common/http';
+import { of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { ProductAddService } from '../services/products-add.service';
 import { ProductAdd } from '../models/products-add';
 import {
@@ -7,16 +10,20 @@ import {
   Validators,
   FormControl,
 } from '@angular/forms';
-
+import { FileUploadService } from '../services/file-upload.service';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { faFileUpload } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-shop-cms-add',
   templateUrl: './shop-cms-add.component.html',
   styleUrls: ['./shop-cms-add.component.scss'],
 })
 export class ShopCmsAddComponent implements OnInit {
-  constructor(private ProductAddService: ProductAddService) {}
+  constructor(
+    private ProductAddService: ProductAddService,
+    private FileUploadService: FileUploadService
+  ) {}
   productAdd: ProductAdd = new ProductAdd();
   form!: FormGroup;
   result: string | undefined;
@@ -24,16 +31,14 @@ export class ShopCmsAddComponent implements OnInit {
   ngOnInit(): void {
     this.form = new FormGroup({
       name: new FormControl('', Validators.required),
-      price: new FormControl('', [
-        Validators.required,
-        Validators.pattern('[0-9]'),
-      ]),
+      price: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required),
       image: new FormControl(''),
     });
   }
   onSubmit(): void {
     this.result = 'success';
+
     this.ProductAddService.addProduct(this.productAdd).subscribe((data) => {
       console.log(data);
     });
